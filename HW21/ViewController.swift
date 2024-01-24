107da704358a2746847e36a07f6253360cb587a2
HristinaBuyanova <hristina-a@mail.ru>
2024-01-24 16:35:40 +0300

    Initial Commit

---
+++ HW21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW21
+//
+//  Created by Христина Буянова on 24.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
